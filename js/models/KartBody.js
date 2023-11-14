e2ba85612f8a6f748d7146d20c8725d327aa713f
Moritz Niesen <niesenmoritz@gmail.com>
2023-11-14 08:32:37 +0100

    Added More Kart Classes

---
+++ js/models/KartBody.js
@@ -0,0 +1,7 @@
+class KartBody extends KartPart{
+    constructor(name, stats, isInwardDrift) {
+        this.name = name;
+        this.stats = stats;
+        this.isInwardDrift = isInwardDrift;
+    }
+}
---
+++ js/models/KartDriver.js
@@ -0,0 +1,7 @@
+class KartDriver extends KartPart {
+    constructor(name, stats, size) {
+        this.name = name;
+        this.stats = stats;
+        this.size = size;
+    }
+}
